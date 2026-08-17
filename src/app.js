43198a676bab532ea8841780dd1334800bac14f2
rathlavathravikumar <ravirathlavath79@gmail.com>
2026-08-17 12:58:00 +0530

    Remove unused debug scripts and fix CORS headers for teacher/student login

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control', 'Pragma'],
 }));
 
 
